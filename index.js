832e9db8ec0c3c17a08d07656e1cfaf907c477ec
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-04-02 20:40:21 -0700

    added inquirer question objs to questions array

--- index.js
+++ index.js
@@ -16,7 +16,49 @@ const createMarkdown = require("utils/generateMarkdown.js");
 //   name: "github",
 //   message: "What is your GitHub username?"
 // }
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "projectname"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What kind of license should your projecy have?",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know abut contributing to the repo?",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests"
+    },
+
+];
 
 inquirer.prompt([])
 // TODO: Write function to synchronously write data in the
